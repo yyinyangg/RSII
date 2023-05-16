e33ac19814d88479a50232f77e529371fdcb7b49
yyinyangg <131518880+yyinyangg@users.noreply.github.com>
2023-05-16 15:19:07 +0200

    Add files via upload

---
+++ system/device.tcl
@@ -0,0 +1,10 @@
+variable CMDLINE_OPTIONS {+define+SIMULATION +define+jc3_toolchain=ise +define+jc3_toolchain_ise +define+jc3_device=XC6SLX16_2CSG324 +define+jc3_device_XC6SLX16_2CSG324 +define+jc3_family=Spartan_6 +define+jc3_family_Spartan_6 +define+jc3_target=SP601 +define+jc3_target_SP601 +incdir+/cad/spartanmc/rs2/19/spartanmc/hardware}
+variable DEVICE XC6SLX16-2CSG324
+variable DEVICE_SIMLIBS {simprims_ver xilinxcorelib_ver unisims_ver unimacro_ver secureip}
+variable DEVICE_SIMLIBS_FUNCTIONAL {simprims_ver xilinxcorelib_ver unisims_ver unimacro_ver secureip}
+variable DEVICE_SIMLIBS_TIMING {simprims_ver xilinxcorelib_ver unisims_ver unimacro_ver secureip}
+variable FAMILY Spartan-6
+variable ROOT_MODULES_FUNCTIONAL {glbl sim_initram testbench}
+variable ROOT_MODULES_POST_PAR {glbl testbench}
+variable TARGET SP601
+variable TOOLCHAIN ise
